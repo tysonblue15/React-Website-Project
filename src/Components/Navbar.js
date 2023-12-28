51faefbda3ab67a9cd2b6d765416603c96c805d1
Tyson Thomas <tysonmthomas@gmail.com>
2023-12-28 00:05:58 -0500

    Edited Font & Fixed Alignment

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import { click } from '@testing-library/user-event/dist/click'
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css';
 import { Button } from './Button';
@@ -19,13 +19,17 @@ function Navbar() {
         }
     };
 
+    useEffect(() => {
+        showButton()
+    }, [])
+
     window.addEventListener('resize', showButton);
 
     return (
     <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                <Link to="/" className="navbar-logo">
+                <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                     TRVL <i className="fab fa-typo3"></i>
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
