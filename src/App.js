63c6cda78f4ed6427147c4eaec9e0db5403a786f
Tyson Thomas <tysonmthomas@gmail.com>
2023-12-28 21:30:21 -0500

    Added Background Video, Sub Buttons, & Sub Pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Navbar from './Components/Navbar';
 import {BrowserRouter as Router, Routes, Route} from 
 'react-router-dom'
 import './App.css';
+import Home from './Components/pages/Home';
+import Services from './Components/pages/Services';
+import Products from './Components/pages/Products';
+import SignUp from './Components/pages/SignUp';
 
 function App() {
   return (
@@ -11,7 +15,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact />
+          <Route path='/' exact Component={Home}/>
+          <Route path='/services' exact Component={Services}/>
+          <Route path='/products' exact Component={Products}/>
+          <Route path='/sign-up' exact Component={SignUp}/>
         </Routes>
       </Router>
     </>
